4c1cee925f18970f2f36912721ea243af5e5c862
thenishantb <nishantbommathiofficial@gmail.com>
2025-09-14 01:48:39 +0530

    Update new.js

--- final/new.js
+++ final/new.js
@@ -1,3 +1,4 @@
+
 const apiKey = "AIzaSyDp21_xVUHK3zsymmitG2N4jWaNa0aeeV8"; 
 const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${apiKey}`;
 
