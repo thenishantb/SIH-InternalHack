f8811e7b933ee76c1d183feeb196a56bee42de05
thenishantb <nishantbommathiofficial@gmail.com>
2025-09-14 01:46:56 +0530

    Add files via upload

---
+++ final/newfaq.js
@@ -0,0 +1,11 @@
+  const faqItems = document.querySelectorAll('.faq-item');
+
+  faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
+    const answer = item.querySelector('.faq-answer');
+
+    question.addEventListener('click', () => {
+      question.classList.toggle('active');
+      answer.classList.toggle('active');
+    });
+  });
